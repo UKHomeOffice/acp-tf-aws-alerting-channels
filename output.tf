8056adea7f2b04f1d2f7cceb4361750e87335067
Chris Freeman <chris.freeman@digital.homeoffice.gov.uk>
2022-04-07 10:57:32 +0100

    slack lambda

--- output.tf
+++ output.tf
@@ -1,11 +1,11 @@
 output "opsgenie_sns_arn" {
-    value = join("", aws_sns_topic.opsgenie.*.arn)
+  value = join("", aws_sns_topic.opsgenie.*.arn)
 }
 
 output "slack_sns_arn" {
-    value = join("", aws_sns_topic.slack.*.arn)
+  value = join("", aws_sns_topic.slack.*.arn)
 }
 
 output "email_sns_arn" {
-    value = join("", aws_sns_topic.email.*.arn)
+  value = join("", aws_sns_topic.email.*.arn)
 }
